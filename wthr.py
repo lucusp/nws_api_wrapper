68631155ef80e39ff931af3d2d732980311c3d73
Lucus Pettigrew <lapettigre@gmail.com>
2021-05-03 10:12:54 -0500

    get rid of unneccessary loop

--- wthr.py
+++ wthr.py
@@ -15,15 +15,14 @@ class weather:
 
         arr = []
 
-        for key in json_data:
-            for items in json_data['features']:
-                obv_time = items['properties']['timestamp']
-                _obv_time = datetime.strptime(obv_time, '%Y-%m-%dT%H:%M:%S+00:00')
-                _obv_time = _obv_time.replace(tzinfo=timezone.utc)
-                local_timezone = pytz.timezone('US/Central')
-                _obv_time = _obv_time.astimezone(local_timezone)
-                date, time = str(_obv_time).split(' ')
-                arr.append({'date': date, 'time': time, 'temp': (items['properties']['temperature']['value']*1.8)+32})
+        for items in json_data['features']:
+            obv_time = items['properties']['timestamp']
+            _obv_time = datetime.strptime(obv_time, '%Y-%m-%dT%H:%M:%S+00:00')
+            _obv_time = _obv_time.replace(tzinfo=timezone.utc)
+            local_timezone = pytz.timezone('US/Central')
+            _obv_time = _obv_time.astimezone(local_timezone)
+            date, time = str(_obv_time).split(' ')
+            arr.append({'date': date, 'time': time, 'temp': (items['properties']['temperature']['value']*1.8)+32})
 
         return arr
 
