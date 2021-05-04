5603ed29c057cdbc6bd64af45757947d3ec5fc23
Lucus Pettigrew <lapettigre@gmail.com>
2021-05-03 22:49:30 -0500

    small re-write for uniform urls and reusability

--- test_weather.py
+++ test_weather.py
@@ -1,5 +1,7 @@
 from wthr import weather as weather
+import pandas as pd
 
 weather = weather()
+df = pd.DataFrame(weather.getHourlyTemps(station='KEVV')['temps'])
 
-print(weather.getHourlyTemps(station='KEVV'))
+print(df.head())
--- wthr.py
+++ wthr.py
@@ -5,23 +5,30 @@ import pytz
 
 class weather:
     def __init__(self):
-        self.base_url = 'https://api.weather.gov'
+        self.base_url = 'https://api.weather.gov/'
         self.headers = {'User-Agent':'Your Name email@email.com'}
 
-    def getHourlyTemps(self, station, time_zone='US/Central',verify=True):
-        req = requests.get(f'{self.base_url}/stations/{station}/observations', verify=verify, headers=self.headers)
+        #observations
+        self.observations_all = 'stations/{station}/observations'.format(station="{station}")
+
+    def __get_data(self, url, verify=True):
+        print(self.observations_all)
+        req = requests.get(self.base_url + url, verify=verify, headers=self.headers)
         json_data = json.loads(req.text)
+        return json_data
 
-        arr = [{'station':f'{station}','local_timezone':f'{time_zone}'}]
+    def getHourlyTemps(self, station, time_zone='US/Central'):
+        data = self.__get_data(self.observations_all.format(station=str(station)))
+        
+        data_dict = {'station':f'{station}', 'local_timezone':f'{time_zone}', 'temps':[]}
 
-        for items in json_data['features']:
+        for items in data['features']:
             obv_time = items['properties']['timestamp']
             _obv_time = datetime.strptime(obv_time, '%Y-%m-%dT%H:%M:%S+00:00')
             _obv_time = _obv_time.replace(tzinfo=timezone.utc)
             local_timezone = pytz.timezone(time_zone)
             _obv_time = _obv_time.astimezone(local_timezone)
             date, time = str(_obv_time).split(' ')
-            arr.append({'date': date, 'time': time, 'temp': (items['properties']['temperature']['value']*1.8)+32})
+            data_dict['temps'].append({'date': date, 'time': time, 'temp': (items['properties']['temperature']['value']*1.8)+32})
 
-        return arr
-
+        return data_dict
