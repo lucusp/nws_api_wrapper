1140e2cad00934d04de5facba876b58788305585
Lucus Pettigrew <lapettigre@gmail.com>
2021-05-06 22:41:52 -0500

    update gitignore

--- test_weather.py
+++
@@ -1,8 +0,0 @@
-from wthr import weather as weather
-import pandas as pd
-
-weather = weather()
-df = pd.DataFrame(weather.getHourlyTemps(station='KEVV')['temps'])
-df['temp'] = df['temp'].round(0)
-
-print(weather.getStationHourlyForecast(station='KEVV'))
